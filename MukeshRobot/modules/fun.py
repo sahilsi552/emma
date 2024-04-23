0e7cd7794f753bd97613cd3016eab7d9b66aee59
SAHIL SURYAVANSHI <sahilsingh56701@gmail.com>
2024-04-23 23:44:53 +0530

    Update fun.py

--- MukeshRobot/modules/fun.py
+++ MukeshRobot/modules/fun.py
@@ -255,7 +255,7 @@ dispatcher.add_handler(DECIDE_HANDLER)
 dispatcher.add_handler(EIGHTBALL_HANDLER)
 dispatcher.add_handler(TABLE_HANDLER)
 
-__mod_name__ = "Fᴜɴ"
+__mod_name__ = "Fᴜɴ 🌟"
 __command_list__ = [
     "runs",
     "slap",
