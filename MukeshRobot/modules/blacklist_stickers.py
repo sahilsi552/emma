32a5efeec5fb7df7db367810e9698982882ab400
SAHIL SURYAVANSHI <sahilsingh56701@gmail.com>
2024-04-24 04:28:18 +0530

    Update blacklist_stickers.py

--- MukeshRobot/modules/blacklist_stickers.py
+++ MukeshRobot/modules/blacklist_stickers.py
@@ -465,7 +465,7 @@ __help__ = """
  ❍ <sticker link > ᴄᴀɴ ʙᴇ `https://t.me/addstickers//<sticker>` ᴏʀ ᴊᴜsᴛ `<sticker>` ᴏʀ ʀᴇᴘʟʏ ᴛᴏ ᴛʜᴇ sᴛɪᴄᴋᴇʀ ᴍᴇssᴀɢᴇ
 """
 
-__mod_name__ = "Sᴛɪᴄᴋᴇʀ"
+__mod_name__ = "Sᴛɪᴄᴋᴇʀ 🕹️"
 
 BLACKLIST_STICKER_HANDLER = DisableAbleCommandHandler(
     "blsticker", blackliststicker, admin_ok=True, run_async=True
