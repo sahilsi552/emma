4a90592cbdbe25385d5ead52985bb4aa71c3b4bd
SAHIL SURYAVANSHI <sahilsingh56701@gmail.com>
2024-04-24 04:25:25 +0530

    Update cust_filters.py

--- MukeshRobot/modules/cust_filters.py
+++ MukeshRobot/modules/cust_filters.py
@@ -615,7 +615,7 @@ __help__ = """
 
 """
 
-__mod_name__ = "Fɪʟᴛᴇʀs"
+__mod_name__ = "Fɪʟᴛᴇʀs 📂"
 
 FILTER_HANDLER = CommandHandler("filter", filters, run_async=True)
 STOP_HANDLER = CommandHandler("stop", stop_filter, run_async=True)
