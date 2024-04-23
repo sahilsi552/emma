5821a4b6a92a1c019eb0fae9e46b5572edbacd3a
SAHIL SURYAVANSHI <sahilsingh56701@gmail.com>
2024-04-23 17:53:09 +0530

    Update config.py

--- MukeshRobot/config.py
+++ MukeshRobot/config.py
@@ -4,13 +4,13 @@ class Config(object):
     # REQUIRED
     # Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = 3595472 # integer value, dont use ""
+    API_ID = "3595472" # integer value, dont use ""
     API_HASH = "1e1f25564047e2994d35ef7785ab6f04"
-    TOKEN = "6985264082:AAFFmCSv6cg2UsAgWE7IU3UL-lZa4ZzCMvQ"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 6728038801 # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    TOKEN = "7178343344:AAE-gvFvekF-8l1gRTQaD6KxyKgf289vqlw"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    OWNER_ID = "6828102589" # If you dont know, run the bot and do /id in your private chat with it, also an integer
     
-    SUPPORT_CHAT = "jihugcgvhjblkbj"  # Your own group for support, do not add the @
-    START_IMG = "https://graph.org//file/9f9f909836f148395399c.jpg"
+    SUPPORT_CHAT = "tyrosupport"  # Your own group for support, do not add the @
+    START_IMG = "https://telegra.ph/file/000625ad0b270aaea4ec6.jpg"
     EVENT_LOGS = ()  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
     MONGO_DB_URI= "mongodb+srv://Mukesh01:mstboy@cluster0.8jwzl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
     # RECOMMENDED
