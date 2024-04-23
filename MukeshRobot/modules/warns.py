42ae4ad0d37ed501bd84b2f906a7dadf95cf060c
SAHIL SURYAVANSHI <sahilsingh56701@gmail.com>
2024-04-24 00:02:04 +0530

    Update warns.py

--- MukeshRobot/modules/warns.py
+++ MukeshRobot/modules/warns.py
@@ -510,7 +510,7 @@ __help__ = """
  ❍ `/strongwarn <ᴏɴ/ʏᴇs/ᴏғғ/ɴᴏ>`*:* ɪғ sᴇᴛ ᴛᴏ ᴏɴ, ᴇxᴄᴇᴇᴅɪɴɢ ᴛʜᴇ ᴡᴀʀɴ ʟɪᴍɪᴛ ᴡɪʟʟ ʀᴇsᴜʟᴛ ɪɴ ᴀ ʙᴀɴ. ᴇʟsᴇ, ᴡɪʟʟ ᴊᴜsᴛ ᴘᴜɴᴄʜ.
 """
 
-__mod_name__ = "Wᴀʀɴs "
+__mod_name__ = "Wᴀʀɴs 🪖"
 
 WARN_HANDLER = CommandHandler(
     ["warn", "dwarn"], warn_user, filters=Filters.chat_type.groups, run_async=True
