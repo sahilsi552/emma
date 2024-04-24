da05afec1f8ddc283d0baa1f2b50db7fccccd0b4
SAHIL SURYAVANSHI <sahilsingh56701@gmail.com>
2024-04-24 05:42:43 +0530

    Update users.py

--- MukeshRobot/modules/users.py
+++ MukeshRobot/modules/users.py
@@ -250,5 +250,5 @@ dispatcher.add_handler(USER_HANDLER, USERS_GROUP)
 dispatcher.add_handler(CHATLIST_HANDLER)
 dispatcher.add_handler(CHAT_CHECKER_HANDLER, CHAT_GROUP)
 
-__mod_name__ = "Usᴇʀs 🙅"
+__mod_name__ = "Usᴇʀs🙅"
 __handlers__ = [(USER_HANDLER, USERS_GROUP), CHATLIST_HANDLER]
