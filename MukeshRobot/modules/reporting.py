919d132ce6d82e1d62ce5d9b4734d097cb32fc7a
SAHIL SURYAVANSHI <sahilsingh56701@gmail.com>
2024-04-24 05:48:43 +0530

    Update reporting.py

--- MukeshRobot/modules/reporting.py
+++ MukeshRobot/modules/reporting.py
@@ -288,7 +288,7 @@ dispatcher.add_handler(SETTING_HANDLER)
 dispatcher.add_handler(REPORT_HANDLER, REPORT_GROUP)
 dispatcher.add_handler(ADMIN_REPORT_HANDLER, REPORT_GROUP)
 
-__mod_name__ = "Rᴇᴘᴏʀᴛs 🔁​"
+__mod_name__ = "Rᴇᴘᴏʀᴛs🔁​"
 __handlers__ = [
     (REPORT_HANDLER, REPORT_GROUP),
     (ADMIN_REPORT_HANDLER, REPORT_GROUP),
