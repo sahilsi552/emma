e67ab9461714731c6910e083231c3025072d620e
SAHIL SURYAVANSHI <sahilsingh56701@gmail.com>
2024-04-30 21:56:32 +0530

    Update memify.py

--- MukeshRobot/modules/memify.py
+++ MukeshRobot/modules/memify.py
@@ -1,74 +1,99 @@
-import textwrap
 import os
-from PIL import Image, ImageFont, ImageDraw
-from MukeshRobot.events import register
-from MukeshRobot import LOGGER, TEMP_DOWNLOAD_DIRECTORY, telethn as bot
+import textwrap
 
+from PIL import Image, ImageDraw, ImageFont
+
+from MukeshRobot import telethn as bot
+from MukeshRobot.events import register
 
 @register(pattern="^/mmf ?(.*)")
 async def handler(event):
     if event.fwd_from:
         return
+
     if not event.reply_to_msg_id:
-        await event.reply("`Provide Some Text To Draw! And Reply To Image/Stickers EXAMPLE: /mmf text`")
+        await event.reply("Provide Some Text To Draw!")
+
         return
+
     reply_message = await event.get_reply_message()
+
     if not reply_message.media:
         await event.reply("```Reply to a image/sticker.```")
+
         return
+
     file = await bot.download_media(reply_message)
-    msg = await event.reply("```Memifying this image! 💠 ```")
+
+    msg = await event.reply("```Memifying this image! ✊🏻 ```")
+
     text = str(event.pattern_match.group(1)).strip()
+
     if len(text) < 1:
-        return await msg.edit("You might want to try `/mmf text`")
+        return await msg.reply("You might want to try `/mmf text`")
+
     meme = await drawText(file, text)
+
     await bot.send_file(event.chat_id, file=meme, force_document=False)
+
     await msg.delete()
+
     os.remove(meme)
-
-
-# Taken from https://github.com/UsergeTeam/Userge-Plugins/blob/master/plugins/memify.py#L64
-# Maybe replyed to suit the needs of this module
 
 
 async def drawText(image_path, text):
     img = Image.open(image_path)
+
     os.remove(image_path)
-    shadowcolor = "black"
+
     i_width, i_height = img.size
+
     if os.name == "nt":
         fnt = "ariel.ttf"
+
     else:
         fnt = "./MukeshRobot/resources/default.ttf"
+
     m_font = ImageFont.truetype(fnt, int((70 / 640) * i_width))
+
     if ";" in text:
         upper_text, lower_text = text.split(";")
+
     else:
         upper_text = text
+
         lower_text = ""
+
     draw = ImageDraw.Draw(img)
+
     current_h, pad = 10, 5
+
     if upper_text:
         for u_text in textwrap.wrap(upper_text, width=15):
-            u_width, u_height = draw.textsize(u_text, font=m_font)
+            uwl, uht, uwr, uhb = m_font.getbbox(u_text)
+            u_width, u_height = uwr - uwl, uhb - uht
+
             draw.text(
                 xy=(((i_width - u_width) / 2) - 2, int((current_h / 640) * i_width)),
                 text=u_text,
                 font=m_font,
                 fill=(0, 0, 0),
             )
+
             draw.text(
                 xy=(((i_width - u_width) / 2) + 2, int((current_h / 640) * i_width)),
                 text=u_text,
                 font=m_font,
                 fill=(0, 0, 0),
             )
+
             draw.text(
                 xy=((i_width - u_width) / 2, int(((current_h / 640) * i_width)) - 2),
                 text=u_text,
                 font=m_font,
                 fill=(0, 0, 0),
             )
+
             draw.text(
                 xy=(((i_width - u_width) / 2), int(((current_h / 640) * i_width)) + 2),
                 text=u_text,
@@ -82,10 +107,14 @@ async def drawText(image_path, text):
                 font=m_font,
                 fill=(255, 255, 255),
             )
+
             current_h += u_height + pad
+
     if lower_text:
         for l_text in textwrap.wrap(lower_text, width=15):
-            u_width, u_height = draw.textsize(l_text, font=m_font)
+            uwl, uht, uwr, uhb = m_font.getbbox(l_text)
+            u_width, u_height = uwr - uwl, uhb - uht
+
             draw.text(
                 xy=(
                     ((i_width - u_width) / 2) - 2,
@@ -95,6 +124,7 @@ async def drawText(image_path, text):
                 font=m_font,
                 fill=(0, 0, 0),
             )
+
             draw.text(
                 xy=(
                     ((i_width - u_width) / 2) + 2,
@@ -104,6 +134,7 @@ async def drawText(image_path, text):
                 font=m_font,
                 fill=(0, 0, 0),
             )
+
             draw.text(
                 xy=(
                     (i_width - u_width) / 2,
@@ -113,6 +144,7 @@ async def drawText(image_path, text):
                 font=m_font,
                 fill=(0, 0, 0),
             )
+
             draw.text(
                 xy=(
                     (i_width - u_width) / 2,
@@ -132,12 +164,18 @@ async def drawText(image_path, text):
                 font=m_font,
                 fill=(255, 255, 255),
             )
+
             current_h += u_height + pad
+
     image_name = "memify.webp"
+
     webp_file = os.path.join(image_name)
+
     img.save(webp_file, "webp")
+
     return webp_file
 
-__mod_name__ = "Mᴍғ🤔"
+
+__mod_name__ = "Mᴍғ"
 __help__ = """ 
 ⫸ /mmf <ᴛᴇxᴛ> ◉ ᴛᴏ ᴍᴇᴍɪғʏ """
