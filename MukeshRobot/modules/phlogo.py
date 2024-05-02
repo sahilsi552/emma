339f353b105d9467c3539e5caf64be666f26a81e
SAHIL SURYAVANSHI <sahilsingh56701@gmail.com>
2024-05-02 09:40:53 +0530

    Create phlogo.py

---
+++ MukeshRobot/modules/phlogo.py
@@ -0,0 +1,51 @@
+import os
+from MukeshRobot import telethn as tbot
+from MukeshRobot.events import register
+from telethon import events
+try:
+	from phlogo import generate
+except ModuleNotFoundError:
+	os.system("pip install phlogo")
+	from phlogo import generate
+
+@register(pattern="^[!/.]phlogo ?(.*)")
+async def ph(event):
+	query = event.pattern_match.group(1)
+	await event.message.delete()
+	if query == "":
+		await event.reply("ɢɪᴠᴇ sᴏᴍᴇ ᴛᴇxᴛ ʙʀᴜʜ, ᴇ.ɢ.: `/phlogo Razer Bot`")
+		return
+	try:
+		p = query.split(" ", 1)[0]
+		h = query.split(" ", 1)[1]
+	except:
+		await event.reply("sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ ᴡʀᴏɴɢ, ᴛʀʏ ɢɪᴠɪɴɢ ᴛᴡᴏ ᴡᴏʀᴅs. ᴇ.ɢ.: `/phlogo porn hub`")
+		return
+	result = generate(f"{p}",f"{h}")
+	pic = "ph.png"
+	result.save(pic, "png")
+	await tbot.send_file(event.chat_id, pic, reply_to=event.reply_to_msg_id, forcedocument=False)
+	os.remove(pic)
+
+
+@register(pattern="^[!/.]phst ?(.*)")
+async def ph(event):
+	query = event.pattern_match.group(1)
+	try:
+		await event.message.delete()
+	except:
+		pass
+	if query == "":
+		await event.reply("ɢɪᴠᴇ sᴏᴍᴇ ᴛᴇxᴛ ʙʀᴜʜ, ᴇ.ɢ.: `/phst Razer Bot`")
+		return
+	try:
+		p = query.split(" ", 1)[0]
+		h = query.split(" ", 1)[1]
+	except:
+		await event.reply("sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ ᴡʀᴏɴɢ, ᴛʀʏ ɢɪᴠɪɴɢ ᴛᴡᴏ ᴡᴏʀᴅs. ᴇ.ɢ.: `/phst Razer Bot`")
+		return
+	result = generate(f"{p}",f"{h}")
+	stc = "ph.webp"
+	result.save(stc, "webp")
+	await tbot.send_file(event.chat_id, stc, reply_to=event.reply_to_msg_id, forcedocument=False)
+	os.remove(stc)
