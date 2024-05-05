25718cf06725bdff1647f29a9ae2d3bcbed5ec54
SAHIL SURYAVANSHI <sahilsingh56701@gmail.com>
2024-05-05 19:07:32 +0530

    Update eval.py

--- MukeshRobot/modules/eval.py
+++ MukeshRobot/modules/eval.py
@@ -155,7 +155,7 @@ dispatcher.add_handler(EVAL_HANDLER)
 dispatcher.add_handler(EXEC_HANDLER)
 dispatcher.add_handler(CLEAR_HANDLER)
 
-__mod_name__ = "Eᴠᴀʟ"
+__mod_name__ = "Eᴠᴀʟ💠"
 __help__ = f"""
 ★ᴏᴡɴᴇʀ ᴄᴍᴅ ★
 ★ /eval :- to evaluate simple code
