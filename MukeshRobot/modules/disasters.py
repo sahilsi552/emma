ab9fea72238fa5084ad12e29e270850a435b2fed
SAHIL SURYAVANSHI <sahilsingh56701@gmail.com>
2024-05-20 09:07:25 +0530

    Update disasters.py

--- MukeshRobot/modules/disasters.py
+++ MukeshRobot/modules/disasters.py
@@ -600,7 +600,7 @@ dispatcher.add_handler(SUPPORTLIST_HANDLER)
 dispatcher.add_handler(SUDOLIST_HANDLER)
 dispatcher.add_handler(DEVLIST_HANDLER)
 
-__mod_name__ = "Dᴇᴠꜱ💀"
+__mod_name__ = "Deᴠꜱ💀"
 __handlers__ = [
     SUDO_HANDLER,
     SUPPORT_HANDLER,
