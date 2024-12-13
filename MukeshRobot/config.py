55a69524440591d94b1fc214dca8810972d04dce
SAHIL SURYAVANSHI <sahilsingh56701@gmail.com>
2024-12-13 12:14:14 +0530

    Update config.py

--- MukeshRobot/config.py
+++ MukeshRobot/config.py
@@ -4,16 +4,16 @@ class Config(object):
     # REQUIRED
     # Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = "3595472" # integer value, dont use ""
-    API_HASH = "1e1f25564047e2994d35ef7785ab6f04"
-    TOKEN = "6891573695:AAHQG-Xj99KlzEZzO42Pfn4svUjZU-oOSgk"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = "6828102589" # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    
+    API_ID = "20892750" # integer value, dont use ""
+    API_HASH = "b0241677a3a2958667e93fa9a632c350"
+    TOKEN = "7694231563:AAEEu-m6eC1vtwTLOAXE6C_qMAs1o2cXZUE"  # This var used tbe API_KEY but it is now TOKEN, adjust accordingly.
+    OWNER_ID = "7785947978" # If you dont know, run the bot and do /id in your rivate chat with it, also an integer
     SUPPORT_CHAT = "tyrosupport"  # Your own group for support, do not add the @
     START_IMG = "https://telegra.ph/file/000625ad0b270aaea4ec6.jpg"
-    EVENT_LOGS = ()  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
-    MONGO_DB_URI= "mongodb+srv://Mukesh01:mstboy@cluster0.8jwzl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    EVENT_LOGS = ()  # Prints information like gbans, sudo promotes, AI enabledisable states that may help in debugging and shit
+    MONGO_DB_URI = "mongodb+srv://SATYAM847108:BIHARDARBHANGAA@cluster0.cfrj9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     # RECOMMENDED
+    DATABASE_URL = "postgres://kykrjayd:s_zaM4ZfJFVIBZzsQxoHi1JKbV5edFgJ@kandula.db.elephantsql.com/kykrjayd"  # A sql database url from elephantsql.com
     DATABASE_URL = "postgres://iadhvjopgtomul:4883b7456515dffbbea0b9e626bd06dbd33cdabfaaa6d1f5117d08c94ff777d6@ec2-18-206-103-49.compute-1.amazonaws.com:5432/dfh9hnujmva3kj"  # A sql database url from elephantsql.com
     CASH_API_KEY = (
         ""  # Get your API key from https://www.alphavantage.co/support/#api-key
