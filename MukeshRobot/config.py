a2969c014a6bf9ee58f73b55220114c68c392de1
SAHIL SURYAVANSHI <sahilsingh56701@gmail.com>
2024-12-13 22:54:55 +0530

    Update config.py

--- MukeshRobot/config.py
+++ MukeshRobot/config.py
@@ -11,9 +11,9 @@ class Config(object):
     SUPPORT_CHAT = "tyrosupport"  # Your own group for support, do not add the @
     START_IMG = "https://telegra.ph/file/000625ad0b270aaea4ec6.jpg"
     EVENT_LOGS = ()  # Prints information like gbans, sudo promotes, AI enabledisable states that may help in debugging and shit
-    MONGO_DB_URI = "mongodb+srv://userbot9:userbot9@cluster0.k3r7k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    MONGO_DB_URI = "mongodb+srv://SATYAM847108:BIHARDARBHANGAA@cluster0.cfrj9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     # RECOMMENDED
-    DATABASE_URL = "postgres://kykrjayd:s_zaM4ZfJFVIBZzsQxoHi1JKbV5edFgJ@kandula.db.elephantsql.com/kykrjayd"  # A sql database url from elephantsql.com
+    DATABASE_URL = "postgres://wgkcxbzo:2-YlvIvuAan7-yUlunTRDYnVVrb2yFAw@kandula.db.elephantsql.com/wgkcxbzo"  # A sql database url from elephantsql.com
     # A sql database url from elephantsql.com
     CASH_API_KEY = (
         ""  # Get your API key from https://www.alphavantage.co/support/#api-key
