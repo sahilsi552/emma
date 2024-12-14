e0e3c33ce31cf15d6568fa451967516141f41a88
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2024-12-14 20:36:41 +0530

    Update chatgpt.py

--- MukeshRobot/modules/chatgpt.py
+++ MukeshRobot/modules/chatgpt.py
@@ -3,22 +3,36 @@ from .. import pbot as Mukesh,BOT_NAME,BOT_USERNAME
 import time
 from pyrogram.enums import ChatAction, ParseMode
 from pyrogram import filters
-from MukeshAPI import api
-@Mukesh.on_message(filters.command(["chatgpt","ai","ask"],  prefixes=["+", ".", "/", "-", "?", "$","#","&"]))
-async def chat_gpt(bot, message):
-    
-    try:
-        await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
-        if len(message.command) < 2:
-            await message.reply_text(
-            "Example:**\n\n`/chatgpt Where is TajMahal?`")
-        else:
-            a = message.text.split(' ', 1)[1]
-            r=api.gemini(a)["results"]
-            await message.reply_text(f" {r} \n\n🎉ᴘᴏᴡᴇʀᴇᴅ ʙʏ @{BOT_USERNAME} ", parse_mode=ParseMode.MARKDOWN)     
-    except Exception as e:
-        await message.reply_text(f"**ᴇʀʀᴏʀ: {e} ")
+AI_MODELS: dict = {
+   "gpt": 1,
+   "claude": 2,
+   "mistral": 3,
+   "meta": 4
+}
 
+
+
+@Mukesh.on_message(filters.command(["gpt", "mistral", "claude", "meta"]))
+async def _AiCmds(_, message):
+     cmd = message.text.split()[0][1:].lower()
+     model_id = AI_MODELS[cmd]
+     if len(message.text.split()) < 2:
+          return await message.reply("**——› No query provided** 😶")
+
+     query = message.text.split(maxsplit=1)[1]
+     data = {
+       "messages": [{ "role": "user", "content": query }],
+       "model_id": model_id
+     }
+
+     msg = await message.reply("✏️")
+     api_url = "https://nandhabots-api.vercel.app/duckai"
+     response = requests.post(api_url, json=data)
+     if response.status_code != 200:
+         return await msg.edit_text(f"[ ❌ ERROR: `{response.reason}`]")
+     else:
+         text = response.json()['reply']
+         return await msg.edit_text(text)
 __mod_name__ = "Cʜᴀᴛɢᴘᴛ🤖"
 __help__ = """
  Cʜᴀᴛɢᴘᴛ ᴄᴀɴ ᴀɴsᴡᴇʀ ʏᴏᴜʀ ǫᴜᴇsᴛɪᴏɴ  ᴀɴᴅ sʜᴏᴡs ʏᴏᴜ ᴛʜᴇ ʀᴇsᴜʟᴛ
